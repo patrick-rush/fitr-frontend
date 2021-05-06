ec7b9910cf98931209079daec48b6ae2f9ca1707
branmar97 <brandonmar97@gmail.com>
2021-05-06 11:43:05 -0400

    add loading workouts case

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -1,6 +1,11 @@
 const manageWorkouts = (state = { list: [], current: {}, loading: false }, action) => {
     switch(action.type) {
-        
+        case 'LOADING_WORKOUTS':
+            return {
+                ...state,
+                list: [...state.list],
+                loading: true
+            }
     }
 }
 
