b59a892b833ba53127c2fab2e02b022fc19fbbcb
branmar97 <brandonmar97@gmail.com>
2021-05-06 12:27:23 -0400

    add create workout action

--- src/actions/workoutActions.js
+++ src/actions/workoutActions.js
@@ -23,3 +23,19 @@ export const fetchWorkout = (id) => {
       })
     }
 }
+
+export const createWorkout = (data) => {
+    return dispatch => {
+      fetch('http://localhost:3001/workouts', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({workout: data}),
+      })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'CREATE_WORKOUT', workout: data })
+      })
+    }
+  }
