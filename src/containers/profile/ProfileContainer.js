9cb3ba4ac84bd536aebed33840a8af6113c5a2f4
patrick-rush <patrick.a.rush@gmail.com>
2021-05-29 10:46:58 -0400

    create home and profile containers

---
+++ src/containers/profile/ProfileContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileContainer = () => {
+    return (
+        <div>
+            Profile Container
+        </div>
+    )
+}
+
+export default ProfileContainer;
