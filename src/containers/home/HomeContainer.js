d2786548574326e50f196b0ff767b7f3a866395a
patrick-rush <patrick.a.rush@gmail.com>
2021-05-29 12:05:40 -0400

    stub out home container functionality

--- src/containers/home/HomeContainer.js
+++ src/containers/home/HomeContainer.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchWorkouts } from '../../actions/workoutActions';
 
-const HomeContainer = () => {
-    return (
-        <div>
-            HomeContainer
-        </div>
-    )
+const HomeContainer = ({ fetchWorkouts, workouts, loading, initialLoad }) => {
+    
+    useEffect(() => {
+        fetchWorkouts();
+    }, [])
+    
+    console.log("workouts =", workouts)
+    
+    if (!initialLoad || loading) {
+        return (
+            <div>Loading Thingy</div>
+        )
+    } else {
+        return (
+            <div>
+                <div>Left Side Container Thingy</div>
+                <div>{workouts[0].user.name}</div>
+                <div>Right Side Container Thingy</div>
+            </div>
+        )
+    }
 }
 
-export default HomeContainer
+const mapStateToProps = state => {
+    return {
+        workouts: state.workouts.list,
+        loading: state.workouts.loading,
+        initialLoad: state.workouts.initialLoad
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchWorkouts: () => dispatch(fetchWorkouts()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
