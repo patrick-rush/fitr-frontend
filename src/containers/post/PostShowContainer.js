2e47a6c66aea4cff817f11858b328221fb1f437b
patrick-rush <patrick.a.rush@gmail.com>
2021-05-29 12:04:18 -0400

    stub out post show content

---
+++ src/containers/post/PostShowContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PostShowContainer = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>Post Info</h1>
+                    <ul>
+                        <li>Author</li>
+                        <li>Post Time</li>
+                    </ul>
+                </div>
+                <h1>Post Content</h1>
+                <div>Likes</div>
+                <ul>
+                    <p>Replys</p>
+                    <li>Reply 1</li>
+                    <li>Reply 2</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default PostShowContainer;
