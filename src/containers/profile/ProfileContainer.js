156ffeb1d4effd69f5640969a41a2cb3bddcfd69
branmar97 <brandonmar97@gmail.com>
2021-05-30 19:40:05 -0400

    add comments to profile container

--- src/containers/profile/ProfileContainer.js
+++ src/containers/profile/ProfileContainer.js
@@ -1,3 +1,11 @@
+// Should get User data from Redux store (avatar, name, bio, age, location)
+// Should get UserPosts from Redux store
+// Should import Widget components
+// Renders header with user data
+// Renders LeftWidgets, Feed and RightWidgets components
+// Pass UserPosts to Feed as props
+// Pass Widget components to LeftWidgets and RightWidgets as props
+
 import React from 'react';
 
 const ProfileContainer = () => {
