bc4656ff37eafe673eab2225e930d21d6422c789
branmar97 <brandonmar97@gmail.com>
2021-06-07 19:45:31 -0400

    add fetch to workouts container

--- src/containers/workouts/WorkoutsContainer.js
+++ src/containers/workouts/WorkoutsContainer.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 const WorkoutsContainer = ({ workouts, loading, fetchWorkouts }) => {
+    useEffect(() => {
+        fetchWorkouts();
+    },[])
+    
     return ( 
         <div>
             LeftWidgets Thingy
